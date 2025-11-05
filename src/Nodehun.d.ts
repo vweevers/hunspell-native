faec03c21b15980fd0c45f71dacac721c4ff86bc
Vincent Weevers <mail@vincentweevers.nl>
2025-11-05 21:34:39 +0100

    Simplify type declaration
    Category: none

--- src/Nodehun.d.ts
+++ src/Nodehun.d.ts
@@ -1,37 +1,35 @@
-declare module 'nodehun-native' {
-  export class Nodehun {
-    constructor(affix: string, dictionary: string);
-    
-    spell(word: string): Promise<boolean>;
-    spellSync(word: string): boolean;
-      
-    suggest(word: string): Promise<string[] | null>;
-    suggestSync(word: string): string[] | null;
-      
-    analyze(word: string): Promise<string[]>;
-    analyzeSync(word: string): string[];
-    
-    stem(word: string): Promise<string[]>;
-    stemSync(word: string): string[];
-    
-    generate(word: string, example: string): Promise<string[]>;
-    generateSync(word: string, example: string): string[];
-  
-    addDictionary(dictionary: string): Promise<void>;
-    addDictionarySync(dictionary: string): void;
-    
-    add(word: string): Promise<void>;
-    addSync(word: string): void;
-    
-    addWithAffix(word: string, example: string): Promise<void>;
-    addWithAffixSync(word: string, example: string): void;
-    
-    remove(word: string): Promise<void>;
-    removeSync(word: string): void;
-    
-    getDictionaryEncoding(): string | undefined;
-    getWordCharacters(): string | undefined;
-    getWordCharactersUTF16(): string | undefined;
-    getVersion(): string | undefined;
-  }  
+export class Nodehun {
+  constructor(affix: string, dictionary: string);
+
+  spell(word: string): Promise<boolean>;
+  spellSync(word: string): boolean;
+
+  suggest(word: string): Promise<string[] | null>;
+  suggestSync(word: string): string[] | null;
+
+  analyze(word: string): Promise<string[]>;
+  analyzeSync(word: string): string[];
+
+  stem(word: string): Promise<string[]>;
+  stemSync(word: string): string[];
+
+  generate(word: string, example: string): Promise<string[]>;
+  generateSync(word: string, example: string): string[];
+
+  addDictionary(dictionary: string): Promise<void>;
+  addDictionarySync(dictionary: string): void;
+
+  add(word: string): Promise<void>;
+  addSync(word: string): void;
+
+  addWithAffix(word: string, example: string): Promise<void>;
+  addWithAffixSync(word: string, example: string): void;
+
+  remove(word: string): Promise<void>;
+  removeSync(word: string): void;
+
+  getDictionaryEncoding(): string | undefined;
+  getWordCharacters(): string | undefined;
+  getWordCharactersUTF16(): string | undefined;
+  getVersion(): string | undefined;
 }
